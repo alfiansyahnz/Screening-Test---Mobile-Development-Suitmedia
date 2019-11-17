175551c7f7ee0c3ca05a550e38df78e4eea92aad
Alfiansyah Nur Zufa <anurzufa@gmail.com>
2019-11-17 14:16:48 +0700

    Task 1 and 2 done

--- app/src/main/java/com/example/suitmedia/EventGuestActivity.java
+++ app/src/main/java/com/example/suitmedia/EventGuestActivity.java
@@ -1,13 +1,53 @@
 package com.example.suitmedia;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class EventGuestActivity extends AppCompatActivity {
+
+    @BindView(R.id.tv_username)
+    TextView tv_username;
+
+    @BindView(R.id.btn_event)
+    Button btn_event;
+
+    @BindView(R.id.btn_guest)
+    Button btn_guest;
+
+    String username;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_guest);
+        ButterKnife.bind(this);
+
+        username = getIntent().getStringExtra("name");
+        tv_username.setText(username);
+
+
+        btn_event.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(EventGuestActivity.this, EventActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btn_guest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(EventGuestActivity.this, GuestActivity.class);
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/suitmedia/MainActivity.java
+++ app/src/main/java/com/example/suitmedia/MainActivity.java
@@ -1,13 +1,43 @@
 package com.example.suitmedia;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.et_name)
+    EditText et_name;
+
+    @BindView(R.id.btn_next)
+    Button btn_next;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        btn_next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = et_name.getText().toString();
+                Toast.makeText(MainActivity.this, "Hello" + name, Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(MainActivity.this, EventGuestActivity.class);
+                intent.putExtra("name", name);
+                startActivity(intent);
+            }
+        });
+
     }
+
+
 }
+
