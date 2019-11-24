62b842083a39f25a2a1fc2e3016d70cfa96ee812
Alfiansyah Nur Zufa <anurzufa@gmail.com>
2019-11-25 03:42:16 +0700

    API key Hidden

--- app/src/main/java/com/example/suitmedia/rest/ApiService.java
+++ app/src/main/java/com/example/suitmedia/rest/ApiService.java
@@ -9,6 +9,6 @@ import retrofit2.http.GET;
 import retrofit2.http.Path;
 
 public interface ApiService {
-    @GET("596dec7f0f000023032b8017")
+    @GET("<api_key>")
     Call<List<Guest>> postData();
 }
